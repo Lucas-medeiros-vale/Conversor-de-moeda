e3dab67f5ca7769482b01ec6affcfab353efa4c4
lucas.vale <lucas.vale@financeiro24horas.com>
2022-08-22 12:01:30 -0300

    conversor-moeda

---
+++ CalculoDolar/CalculoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoDolar
+{
+	class ConversorDeMoeda
+	{
+		public static double Iof = 6.0;
+
+		public static double DolarParaReal(double cotacao, double quantia)
+		{
+			double total = quantia * cotacao;
+			return total + total * Iof / 100.0;
+		}
+	}
+}
